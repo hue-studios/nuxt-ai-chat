820f81add9ac77e1e6fac1172eb6d8d006b83fbc
pvenableh <peter@huestudios.com>
2023-07-19 12:16:15 -0400

    Update two.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	css: ['~/assets/css/main.css'],
+	nitro: {
+		preset: "vercel",
+	  },
 	runtimeConfig: {
 		OPENAI_API_KEY: process.env.OPENAI_API_KEY
 	},
+	modules: ["nuxt-chatgpt"],
+	chatgpt: {
+		apiKey: process.env.OPENAI_API_KEY
+	  },
 	postcss: {
 		plugins: {
 			tailwindcss: {},
